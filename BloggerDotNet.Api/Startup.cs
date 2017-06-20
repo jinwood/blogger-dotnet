2a8f8f78b45aded66df962245ba2514f0c0892b8
Julian <jinw@protonmail.com>
2017-06-20 15:57:43 +0100

    Add api project. Configure DI

---
+++ BloggerDotNet.Api/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Mvc.ViewFeatures.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using SimpleInjector;
+using SimpleInjector.Integration.AspNetCore.Mvc;
+using SimpleInjector.Lifestyles;
+
+namespace BloggerDotNet.Api
+{
+    public class Startup
+    {
+        private Container container = new Container();
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+
+            services.AddSingleton<IControllerActivator>(
+            new SimpleInjectorControllerActivator(container));
+            services.AddSingleton<IViewComponentActivator>(
+                new SimpleInjectorViewComponentActivator(container));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+        }
+
+        private void InitializeContainer(IApplicationBuilder app)
+        {
+            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+
+            // Add application presentation components:
+            container.RegisterMvcControllers(app);
+            container.RegisterMvcViewComponents(app);
+
+            // Add application services. For instance:
+            //container.Register<IUserRepository, SqlUserRepository>(Lifestyle.Scoped);
+
+            // Cross-wire ASP.NET services (if any). For instance:
+            container.RegisterSingleton(app.ApplicationServices.GetService<ILoggerFactory>());
+
+            // The following registers a Func<T> delegate that can be injected as singleton,
+            // and on invocation resolves a MVC IViewBufferScope service for that request.
+            container.RegisterSingleton<Func<IViewBufferScope>>(
+                () => app.GetRequestService<IViewBufferScope>());
+        }
+    }
+}
