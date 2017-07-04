7e49e943cdb5f144344a1838432d19bdc033b171
Julian <jinw@protonmail.com>
2017-07-04 11:41:29 +0100

    Add placeholder repository implementation

--- BloggerDotNet.Core/Interfaces/IPostRepository.cs
+++ BloggerDotNet.Core/Interfaces/IPostRepository.cs
@@ -6,7 +6,7 @@ namespace BloggerDotNet.Core.Interfaces
 {
     public interface IPostRepository
     {
-        Task<List<Post>> GetAllPosts();
+        Task<List<Post>> GetAllPublishedPosts();
         Task<Post> GetPostByReference(string reference);
         Task<Post> CreatePost(Post post);
         Task<bool> DeletePost(string reference);
